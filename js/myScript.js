ae671616358c216309de25b338840ec35f216598
Vitaliy <kapron4ik@mail.ru>
2019-06-02 23:36:57 +0300

    change work

--- js/myScript.js
+++ js/myScript.js
@@ -1,24 +1,24 @@
-let listAgrotechnics = `
-<h5>Цели и предполагаемые задачи внедрения:</h5></br>
-<ul>
-    <li>Контроль местоположения и состояния автомобиля в Украине и за ее пределами</li></br>
-    <li>Контроль расхода топлива</li></br>
-    <li>Контроль режимов работы Получаемый результат от предлагаемого решения:</li></br>
-</ul>
-<h5>Получаемый результат от предлагаемого решения:</h5></br>
-<ul>
-    <li>Контроль местоположения автомобиля в реальном времени по всему миру</li></br>
-    <li>Контроль уровня и расхода топлива</li></br>
-</ul>`;
+// let listAgrotechnics = `
+// <h5>Цели и предполагаемые задачи внедрения:</h5></br>
+// <ul>
+//     <li>Контроль местоположения и состояния автомобиля в Украине и за ее пределами</li></br>
+//     <li>Контроль расхода топлива</li></br>
+//     <li>Контроль режимов работы Получаемый результат от предлагаемого решения:</li></br>
+// </ul>
+// <h5>Получаемый результат от предлагаемого решения:</h5></br>
+// <ul>
+//     <li>Контроль местоположения автомобиля в реальном времени по всему миру</li></br>
+//     <li>Контроль уровня и расхода топлива</li></br>
+// </ul>`;
 
-function target () {
-    let parent = event.target.closest('li');
-    let nodeVS = parent.querySelector('.infoList');
-    // parent.appendChild(listAgrotechnics);
-    nodeVS.innerHTML = listAgrotechnics;
-    console.log ('node', nodeVS)
-    console.log(event.target.closest('li'));
-}
+// function target () {
+//     let parent = event.target.closest('li');
+//     let nodeVS = parent.querySelector('.infoList');
+//     // parent.appendChild(listAgrotechnics);
+//     nodeVS.innerHTML = listAgrotechnics;
+//     console.log ('node', nodeVS)
+//     console.log(event.target.closest('li'));
+// }
 
 let accordion = document.getElementById('accordion');
 let currentElem = null;
@@ -33,7 +33,10 @@ accordion.onmouseover = function(event) {
     if (target == this) return;
     currentElem = target;
     let node = target.querySelector('.infoList');
-    node.innerHTML = listAgrotechnics;
+    node.style.display = "block";
+    let title = currentElem.querySelector('.title');
+    title.style.display = "none";
+    // node.innerHTML = listAgrotechnics;
 };
   
 accordion.onmouseout = function(event) {
@@ -46,6 +49,9 @@ accordion.onmouseout = function(event) {
         }
     }   
     let node = currentElem.querySelector('.infoList');
-    node.innerHTML = '';
+    node.style.display = "none";
+    let title = currentElem.querySelector('.title');
+    title.style.display = "block";
+    // node.innerHTML = '';
     currentElem = null;
 };
